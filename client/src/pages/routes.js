8a7c19f8fd0b110a7ddd911f6dfa0f7ec915e3b3
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-06-03 13:36:56 +0430

    Add Products Page To Router

--- client/src/pages/routes.js
+++ client/src/pages/routes.js
@@ -4,6 +4,7 @@ import Homepage from './homepage/homepage'
 import ContactUs from './contact-us/contact-us'
 import LogIn from './log-in/log-in'
 import SignUp from './sign-up/sign-up'
+import Products from './products/products'
 
 const Routes = () =>{
     return(
@@ -12,6 +13,7 @@ const Routes = () =>{
             < Route path="/Contact-Us" component={ContactUs} />
             < Route path="/Log-In" component={LogIn} />
             < Route path="/Sign-Up" component={SignUp} />
+            < Route path="/Products" component={Products} />
         </Switch>
     )
 }
